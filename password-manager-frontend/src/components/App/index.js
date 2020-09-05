e6c9f9525e8a32042ceb495dbdde018c659467cb
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-09-04 20:18:09 -0600

    React app structure

---
+++ password-manager-frontend/src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+
+
+const App = () => (
+    <div className = "App">
+        <div>
+            <p>Hello puto</p>
+        </div>
+    </div>
+);
+
+export default App;
